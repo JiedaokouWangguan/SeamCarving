5a2c63bc17479e246ea2099ff3f071bd8fba426d
JiedaokouWangguan <ysong243@wisc.edu>
2018-02-02 10:47:07 -0600

    update

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from seamCarver.seam_carver import SeamCarver
 
 sc = SeamCarver("painting.jpg", "modified_painting.jpg", 0.6, 0.4)
 rows, cols = sc.get_rows_cols()
-pic = sc.carve(rows+50, cols + 300)  # all the argument errors
+pic = sc.carve(rows+50, cols + 300)
